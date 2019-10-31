4231364250678af636240f01a1f70d94c3638fec
Daniel <fleancu.daniel@gmail.com>
2019-10-31 22:24:24 +0200

    fix bugs

--- app/Http/Controllers/Pay/PaymentWebhookController.php
+++ app/Http/Controllers/Pay/PaymentWebhookController.php
@@ -8,6 +8,8 @@ use App\Notifications\NewTips;
 use App\Utils\Payments\VisaPayment;
 use App\Models\PaymentLogResponse; 
 use App\Models\Tips; 
+use App\Models\WithdrawTips; 
+use App\Utils\Payments\WithdrawalService; 
 
 class PaymentWebhookController extends Controller
 {
@@ -17,36 +19,50 @@ class PaymentWebhookController extends Controller
   
 		if ($request->Order_Id) 
 		{
-			$tip = Tips::where('rand', $request->Order_Id)->first();
-			if ($tip) 
-			{
-
-				if ($request->Event == 'Payment' && $request->Status == 'BLOCKED') 
-				{ 
-					$paymentClass = new VisaPayment; 
-		    		$data = $paymentClass->setTranId($request->Transaction_Id)
-		    		                     ->setAmount(toFloat($tip->amount))
-		    		                     ->makeCharge();
-
-		    		$this->log($data); 
-				}
-
-				$tip->status         = $request->Status;
-				$tip->rrn            = @$request->RRN;
-				$tip->id_transaction = $request->Transaction_Id;
-
-				if (!empty($request->NewAmount)) 
+			if ($request->Event == 'Payment') 
+			{ 
+				$tip = Tips::where('rand', $request->Order_Id)->first();
+				if ($tip) 
 				{
-					$tip->amount = toFloat($request->NewAmount); 
+
+					if ($request->Event == 'Payment' && $request->Status == 'BLOCKED') 
+					{ 
+						$paymentClass = new VisaPayment; 
+			    		$data = $paymentClass->setTranId($request->Transaction_Id)
+			    		                     ->setAmount(toFloat($tip->amount))
+			    		                     ->makeCharge();
+
+			    		$this->log($data); 
+					}
+
+					$tip->status         = $request->Status;
+					$tip->rrn            = @$request->RRN;
+					$tip->id_transaction = $request->Transaction_Id;
+
+					if (!empty($request->NewAmount)) 
+					{
+						$tip->amount = toFloat($request->NewAmount); 
+					} 
+	 
+					$tip->save();
+
+					if ($request->Status == 'CHARGED') 
+					{
+						$tip->user->notify(new NewTips($tip->amount));
+					}
 				} 
- 
-				$tip->save();
-
-				if ($request->Status == 'CHARGED') 
-				{
-					$tip->user->notify(new NewTips($tip->amount));
+			}
+			elseif ($request->Event == 'Payout') 
+			{
+				$withdraw = WithdrawTips::where('rand', $request->Order_Id)->first();
+				if ($withdraw) 
+				{ 
+					$withdraw->status         = ($request->Status == 'CHARGED') ? 'SUCCESS' : ''; 
+					$withdraw->id_transaction = $request->Transaction_Id;
+       				$withdraw->pan_ref_token  = $request->panRefToken ?: ''; 
+       				$withdraw->save();
 				}
-			} 
+			}
 		} 
 	}
 
@@ -54,7 +70,7 @@ class PaymentWebhookController extends Controller
 	{
 		PaymentLogResponse::create([
 			'order_rand'   => !empty($data->Order_Id) ? $data->Order_Id : '',
-			'payment_mode' => !empty($data->IsTest) ? 'production' : 'dev',
+			'payment_mode' => !empty($data->IsTest) ? 'dev' : 'production',
 			'flag'         => !empty($data->Status) ? $data->Status : '', 
 			'log'          => json_encode($data)
 		]);
