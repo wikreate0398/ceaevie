f5babc9f49a2e1ee48fe5558f5c1fe4bb70d7a73
Daniel <fleancu.daniel@gmail.com>
2019-11-19 21:41:15 +0200

    tet

--- resources/views/profile/utils/cards.blade.php
+++ resources/views/profile/utils/cards.blade.php
@@ -17,14 +17,7 @@
                 <h4 class="font-weight-normal mb-3">
                     Заработано "чаевых" всего
                 </h4>
-                <h2> 
-                    
-                    @if(false)
-                        {{ sumTipAmount(\App\Models\Tips::confirmed()
-                                                     ->selectRaw('amount, status, created_at, location_work_type, id_location, location_amount')
-                                                     ->where(((\Auth::user()->type == 'admin') ? 'id_location' : 'id_user'), \Auth::id())
-                                                     ->get(), Auth::user()->type, Auth::id()) }} Р
-                    @endif
+                <h2>  
                     {{ \Ballance::getUserBallance(Auth::id(), Auth::user()->type) }} Р
                 </h2>
             </div>
@@ -37,13 +30,7 @@
                 <h4 class="font-weight-normal mb-3">
                     Заработано "чаевых" на этой неделе
                 </h4>
-                <h2>
-                    @if(false)
-                        {{ sumTipAmount(\App\Models\Tips::confirmed(7)
-                                                 ->selectRaw('amount, status, created_at, location_work_type, id_location, location_amount')
-                                                 ->where(((\Auth::user()->type == 'admin') ? 'id_location' : 'id_user'), \Auth::id())
-                                                 ->get(), Auth::user()->type, Auth::id()) }} Р
-                    @endif
+                <h2> 
                     {{ \Ballance::getUserBallance(Auth::id(), Auth::user()->type, 7) }} Р
                 </h2>
             </div>
